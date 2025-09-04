96912925797bba88f1c46c77e6139c28ed1a630b
Richard Campbell <richard.campbell@appliedoceansciences.com>
2025-09-03 21:45:25 -0700

    skip reopening

--- finalize_wav.py
+++ finalize_wav.py
@@ -3,13 +3,11 @@ import sys
 import struct
 import os
 
-f = open(sys.argv[1], 'rb')
+f = open(sys.argv[1], 'r+b')
 
 riff, filesize_minus_eight_bytes, wave = struct.unpack('4sI4s', f.read(12))
 if riff != b'RIFF' or wave != b'WAVE': exit(1)
-f.close()
 
-f = open(sys.argv[1], 'ab')
 f.truncate(filesize_minus_eight_bytes + 8)
 f.close()
 
