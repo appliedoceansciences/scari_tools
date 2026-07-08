83c6d1a579a4a7d188643294f4d508c016d50d72
Richard Campbell <richard.campbell@appliedoceansciences.com>
2026-07-08 11:55:12 -0700

    if desired frequency is given, print the level of a single frequency bin closest to it

--- print_levels_from_json.py
+++ print_levels_from_json.py
@@ -2,14 +2,26 @@
 import sys
 import base64
 import json
+import math
 import numpy as np
+
+def bin_index_given_frequency(frequency, df, bins_per_octave):
+    linear_bins_from_dc = math.ceil(bins_per_octave / math.log(2))
+    linear_index_from_dc = frequency / df
+
+    if linear_index_from_dc < linear_bins_from_dc:
+        return linear_index_from_dc - 2
+    else:
+        return linear_bins_from_dc - 2 + math.log2(linear_index_from_dc / linear_bins_from_dc) * bins_per_octave
 
 def main():
     full_scale = 0
+    desired_bin_frequency = None
 
     # loop over pairs of arguments
     for key, value in zip(sys.argv[1::2], sys.argv[2::2]):
         if key == 'full_scale': full_scale = float(value)
+        if key == 'freq': desired_bin_frequency = float(value)
 
     for line in sys.stdin:
         try: message = json.loads(line)
@@ -21,13 +33,20 @@ def main():
             try: pixels = base64.b64decode(message['pgram'])
             except: continue
 
+            df = float(message['df'])
+            bins_per_octave = int(message['bins_per_octave'])
+
             chigh = 0
             cstep = 0.75
             clow = chigh - 256.0 * cstep
 
             spl_dB = np.frombuffer(pixels, dtype=np.uint8) * cstep + clow + full_scale
 
-            np.savetxt(sys.stdout, np.expand_dims(spl_dB, axis=0), fmt='%.1f', delimiter=', ')
+            if desired_bin_frequency is not None:
+                print(spl_dB[round(bin_index_given_frequency(desired_bin_frequency, df, bins_per_octave))])
+            else:
+                np.savetxt(sys.stdout, np.expand_dims(spl_dB, axis=0), fmt='%.1f', delimiter=', ')
+
             sys.stdout.flush()
 
 main()
